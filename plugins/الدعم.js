e0c3fe3bd5911bda7142bd971f788dd181fcfee5
shadow-bot01 <odrmod@gmail.com>
2024-05-12 17:37:20 +0300

    Create الدعم.js

---
+++ plugins/الدعم.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+let handler = async (m, {command, conn, usedPrefix}) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venom.json`)).data  
+let haha = await res[Math.floor(res.length * Math.random())]    
+conn.sendFile(m.chat, haha, 'error.jpg', `
+
+*『 ️اليك قائمه بسورس البوت البوت 』*
+
+*『 ️واتساب 』*
+
+*⊱≼ https://wa.me/+201153573240 ≽⊰⊹*
+
+*『 ️منصاتي 』*
+
+*⊱≼ غير متوفر ≽⊰⊹*
+
+*『 ️جروب واتساب 』*
+
+*⊱≼ SOON ≽⊰⊹*
+
+*◉═══ • ❁ 👑 ❁ • ═══◉* `, m)
+}
+handler.command = handler.help = ['دعم','الدعم']
+handler.tags = ['kaneki']
+export default handler
